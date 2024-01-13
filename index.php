3fe353f366f10d33e0bca1329c704b2002ead4f5
AndreyShyshkin <shyshkinandrey06@gmail.com>
2024-01-13 16:44:48 +0200

    updateHomePage

--- index.php
+++ index.php
@@ -5,7 +5,17 @@
 </head>
 
 <body>
+	<?php
+	$sql = "SELECT * FROM `articles`";
+	$result = mysqli_query($conn, $sql);
 
+	while ($row = $result->fetch_assoc()) {
+		echo $row["title"];
+		?><a href="<?php echo $row["link"]; ?>">link</a></br>
+		<?php
+	}
+
+	?>
 	<?php require $_SERVER["DOCUMENT_ROOT"] . "/module/scripts.php"; ?>
 </body>
 
