068ee43466f7f68d746e492916a5f31cf70eb2ba
AndreyShyshkin <shyshkinandrey06@gmail.com>
2024-01-13 13:53:15 +0200

    start

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+
+<head>
+	<?php require $_SERVER["DOCUMENT_ROOT"] . "/partials/head.php"; ?>
+</head>
+
+<body>
+
+	<?php require $_SERVER["DOCUMENT_ROOT"] . "/partials/scripts.php"; ?>
+</body>
+
+</html>
---
+++ module/head.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8">
+<title>Wiki</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/assets/css/main.styles.css">
+<?php
+require $_SERVER["DOCUMENT_ROOT"] . "/config/DB_connect.php";
+?>
